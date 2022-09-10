7b91fa96174e28df496e07f1f6ce777d44fb0461
Martin Zahnd <mzahnd@itba.edu.ar>
2022-09-10 20:00:19 -0300

    Fix command and output issues * sed (in COMMAND_PARSER) sometimes found more than one blank space after   the hash and did not replace it with the desired string. * All variables are now initialized before using to avoid undesired   characters appearing on the output. * MAX_COMMAND_OUTPUT_LENGHT increased to allow longer file paths

--- src/slave/slave.c
+++ src/slave/slave.c
@@ -1,11 +1,10 @@
 #define _XOPEN_SOURCE 500
 
 #define MAX_COMMAND_LENGTH 1024
-#define MAX_COMMAND_OUTPUT_LENGTH 64
+#define MAX_COMMAND_OUTPUT_LENGTH 128
 #define COMMAND_STR "md5sum"
-#define COMMAND_PARSER "sed -r 's/ .*\..*/ - File Name:&/g' | sed 's/\n/\"\"/'"
+#define COMMAND_PARSER "sed -r 's/[[:blank:]]+.*$/ - File Name:&/g'"
 
-// #include <slave.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -25,18 +24,20 @@ int main(int argc, char const *argv[])
 
 void do_task(char *file_name)
 {
-        char command[MAX_COMMAND_LENGTH];
-        sprintf(command, "md5sum %s | sed -r 's/ .*\\..*/ - File Name:&/g'",
-                file_name);
-        FILE *output = popen(command, "r");
+        FILE *output = NULL;
+        char command[MAX_COMMAND_LENGTH] = { 0 };
+        char result[MAX_COMMAND_OUTPUT_LENGTH * sizeof(char)] = { 0 };
 
+        snprintf(command, MAX_COMMAND_LENGTH, "%s %s | %s", COMMAND_STR,
+                 file_name, COMMAND_PARSER);
+
+        output = popen(command, "r");
         if (output == NULL) {
                 fprintf(stderr, "ERROR: could not execute md5 on file %s",
                         file_name);
-                exit(1);
+                exit(EXIT_FAILURE);
         }
 
-        char result[MAX_COMMAND_OUTPUT_LENGTH];
         fread(result, sizeof(char), MAX_COMMAND_OUTPUT_LENGTH, output);
         printf("PID: %d - md5: %s", getpid(), result);
 }
@@ -44,12 +45,14 @@ void do_task(char *file_name)
 void wait_more_tasks()
 {
         char buff[MAX_COMMAND_LENGTH] = { 0 };
-        int dim;
+        int dim = 0;
         while ((dim = read(STDIN_FILENO, buff, MAX_COMMAND_LENGTH)) > 0) {
-                // Esto es porque lo hago a mano con la terminal, por lo que 
-                // tengo que apretar enter y agrega un \n. 
+                // Esto es porque lo hago a mano con la terminal, por lo que
+                // tengo que apretar enter y agrega un \n.
                 // Posiblemente haya que sacarlo
-                buff[dim - 1] = 0;
+                if (buff[dim - 1] == '\n') {
+                        buff[dim - 1] = 0;
+                }
                 do_task(buff);
                 memset(buff, 0, dim);
         }
