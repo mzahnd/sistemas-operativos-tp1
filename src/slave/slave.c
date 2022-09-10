4406ce79e024540d0237a5e305c72e12845513d8
Martin Zahnd <mzahnd@itba.edu.ar>
2022-09-10 18:33:02 -0300

    Fix code style

--- src/slave/slave.c
+++ src/slave/slave.c
@@ -12,38 +12,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-void do_task(char * file_name);
+void do_task(char *file_name);
 void wait_more_tasks();
 
-int main(int argc, char const* argv[]) {
-    for (int i = 1; i < argc; i++) {
-        do_task((char *)argv[i]);
-    }
-    wait_more_tasks();
+int main(int argc, char const *argv[])
+{
+        for (int i = 1; i < argc; i++) {
+                do_task((char *)argv[i]);
+        }
+        wait_more_tasks();
 }
 
-void do_task(char * file_name) {
-    char command[MAX_COMMAND_LENGTH];
-    sprintf(command, "md5sum %s | sed -r 's/ .*\\..*/ - File Name:&/g'", file_name);
-    FILE * output = popen(command, "r");
+void do_task(char *file_name)
+{
+        char command[MAX_COMMAND_LENGTH];
+        sprintf(command, "md5sum %s | sed -r 's/ .*\\..*/ - File Name:&/g'",
+                file_name);
+        FILE *output = popen(command, "r");
 
-    if (output == NULL) {
-        fprintf(stderr, "ERROR: could not execute md5 on file %s", file_name);
-        exit(1);
-    }
+        if (output == NULL) {
+                fprintf(stderr, "ERROR: could not execute md5 on file %s",
+                        file_name);
+                exit(1);
+        }
 
-    char result[MAX_COMMAND_OUTPUT_LENGTH];
-    fread(result, sizeof(char), MAX_COMMAND_OUTPUT_LENGTH, output);
-    printf("PID: %d - md5: %s", getpid(), result);
+        char result[MAX_COMMAND_OUTPUT_LENGTH];
+        fread(result, sizeof(char), MAX_COMMAND_OUTPUT_LENGTH, output);
+        printf("PID: %d - md5: %s", getpid(), result);
 }
 
-void wait_more_tasks() {
-    char buff[MAX_COMMAND_LENGTH] = {0};
-    int dim;
-    while((dim = read(STDIN_FILENO, buff, MAX_COMMAND_LENGTH)) > 0) {
-        buff[dim-1] = 0; // Esto es porque lo hago a mano con la terminal, por lo que tengo que apretar enter y agrega un \n. Posiblemente haya que sacarlo
-        do_task(buff);
-        memset(buff, 0, dim);
-    }
+void wait_more_tasks()
+{
+        char buff[MAX_COMMAND_LENGTH] = { 0 };
+        int dim;
+        while ((dim = read(STDIN_FILENO, buff, MAX_COMMAND_LENGTH)) > 0) {
+                // Esto es porque lo hago a mano con la terminal, por lo que 
+                // tengo que apretar enter y agrega un \n. 
+                // Posiblemente haya que sacarlo
+                buff[dim - 1] = 0;
+                do_task(buff);
+                memset(buff, 0, dim);
+        }
 }
